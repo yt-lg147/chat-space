25620f9107c54c6d54a56c953a1d3733cb11018d
yt-lg147 <yuu_takaoka@hotmail.com>
2018-05-10 04:42:39 +0900

    Add @set_group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+before_action :set_group, only: [:edit, :update]
 
 def new
   @group = Group.new
@@ -15,13 +16,24 @@ def create
 end
 
 def edit
-  @group = Group.find(params[:id])
+end
+
+def update
+  if @group.update(group_params)
+    redirect_to root_path, notice: "グループを編集しました"
+  else
+    render :edit
+  end
 end
 
 private
 
 def group_params
   params.require(:group).permit(:group_name, {user_ids: []})
+end
+
+def set_group
+  @group = Group.find(params[:id])
 end
 
 end
