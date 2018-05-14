4cdcd5fc381aed2559f8e511d062532273529f92
yt-lg147 <yuu_takaoka@hotmail.com>
2018-05-14 21:51:08 +0900

    Add messages_controller#index,#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @message = Message.new
+    @messages = @group.messages.all.includes(:user)
+  end
+
+  def create
+    @message = Message.new(post_params)
+    if @message.save
+      redirect_to group_messages_path(params[:group_id])
+    else
+      redirect_to group_messages_path(params[:group_id]), alert: "メッセージを入力してください"
+    end
+  end
+
+private
+  def post_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
+
