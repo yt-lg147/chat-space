47a30f52522babe252af14d3612aa18766129bdd
yt-lg147 <yuu_takaoka@hotmail.com>
2018-05-14 21:43:48 +0900

    Add @japan_time(messages created_at japan time )

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,12 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :content, presence: true, unless: :image?
+  validates :body, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader
+
+  def japan_time(created_at)
+    t1 = created_at + 9.hours
+    t2 = t1.strftime('%Y/%m/%d %H:%M:%S')
+    return t2
+  end
 end
