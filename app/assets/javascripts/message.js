821aa588e1954cb1a878fb0774c53a96a169afb9
yt-lg147 <yuu_takaoka@hotmail.com>
2018-05-31 16:23:37 +0900

    【追加】ajaxによるmessage#createアクション実行

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,6 +5,21 @@ $(function() {
     var $this = $(this);
     var url = $(this).attr('action');
     var formData = new FormData($this.get(0));
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: 'json',
+      data: formData,
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
       console.log("イベント発火確認");
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.log("XMLHttpRequest : " + jqXHR.status);
+      console.log("textStatus     : " + textStatus);
+      console.log("errorThrown    : " + errorThrown.message);
+    })
   });
 });
