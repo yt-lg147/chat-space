6a8cee13924bb3591940fba6859d0958df1332b6
yt-lg147 <yuu_takaoka@hotmail.com>
2018-05-31 16:20:16 +0900

    【追加】form入力内容取得

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,6 +2,9 @@ $(function() {
 
   $(".new_message").on("submit", function(event) {
     event.preventDefault();
+    var $this = $(this);
+    var url = $(this).attr('action');
+    var formData = new FormData($this.get(0));
       console.log("イベント発火確認");
   });
 });
