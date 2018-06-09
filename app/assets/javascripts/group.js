d143b7be21ccfb26ad870397ecd7fe5039534eda
yt-lg147 <yuu_takaoka@hotmail.com>
2018-06-10 00:53:52 +0900

    【変更】入力後のajax処理の追加

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -2,5 +2,23 @@ $(function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          console.log(user.name);
+        });
+      }
+    })
+    .fail(function() {
+      alert("ユーザー検索に失敗しました");
+    })
   });
+
 });
