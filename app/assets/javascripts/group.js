8108ae640996b649703e0ee8c98c499dc5b0fb9f
yt-lg147 <yuu_takaoka@hotmail.com>
2018-06-10 00:56:11 +0900

    【変更】検索結果リストをviewに追記

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,5 +1,15 @@
 $(function() {
 
+  var user_list = $("#user-search-result");
+
+  function searchUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
+                </div>`
+    user_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
@@ -12,7 +22,7 @@ $(function() {
       $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
-          console.log(user.name);
+          searchUser(user);
         });
       }
     })
