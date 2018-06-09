485475b57bd4017b23a4b59f6d63bc32db7562ca
yt-lg147 <yuu_takaoka@hotmail.com>
2018-06-10 01:03:19 +0900

    【変更】検索結果リストから追加ボタンクリック時のjs動作を追加

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,6 +1,7 @@
 $(function() {
 
   var user_list = $("#user-search-result");
+  var select_user = $("#chat-group-users");
 
   function searchUser(user) {
     var html = `<div class="chat-group-user clearfix">
@@ -8,6 +9,17 @@ $(function() {
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
                 </div>`
     user_list.append(html);
+  }
+
+  function appendUser(user) {
+    var user_id = $(user).attr("data-user-id");
+    var user_name = $(user).attr("data-user-name");
+    var html = `<div class='chat-group-user clearfix js-chat-member'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    select_user.append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
@@ -34,6 +46,10 @@ $(function() {
     .fail(function() {
       alert("ユーザー検索に失敗しました");
     })
+  });
+
+  $(document).on("click", ".user-search-add", function(){
+    appendUser(this);
   });
 
 });
